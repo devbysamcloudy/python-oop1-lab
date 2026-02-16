e133bcbaa34c0fc2c61c70cfcd18fe47ecf8d3b8
devbysamcloudy <snganga685@gmail.com>
2026-02-16 08:42:12 +0300

    updated

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,22 @@
-#!/usr/bin/env python3
-
 class Coffee:
-    pass
+    VALID_SIZES = ["Small", "Medium", "Large"]
+
+    def __init__(self, size, price):
+        self._size = None
+        self.size = size   # validation
+        self.price = price
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if value in Coffee.VALID_SIZES:
+            self._size = value
+        else:
+            print("size must be Small, Medium, or Large")
+
+    def tip(self):
+        print("This coffee is great, here’s a tip!")
+        self.price += 1
